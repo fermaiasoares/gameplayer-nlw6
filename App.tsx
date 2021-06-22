db00966e8f1288e2fb863644f0e75cfd31432c9c
Fernando Maia <fermaiasoares@aol.com>
2021-06-21 22:52:36 -0300

    Fonts Personalizadas

--- App.tsx
+++ App.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+
 import SignIn from './src/screens/SignIn';
+import { Text, View } from 'react-native';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  })
+
+  if(!fontsLoaded) {
+    return <View><Text>Carregando...</Text></View>
+  }
+
   return <SignIn />
 }
